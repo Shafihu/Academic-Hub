261f5323a6c4b6e4b823765a3ed973cfad5f75dc
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-05-23 09:19:48 +0000

    teachers page: ui and minor bug fixes

--- src/app/dashboard/teachers/page.jsx
+++ src/app/dashboard/teachers/page.jsx
@@ -5,10 +5,12 @@ import React, { useState, useEffect } from 'react';
 import { BiSolidSearchAlt2 } from "react-icons/bi";
 import mockTeachersData from '@/utils/mockTeachersData';
 
-const page = () => {
+const Page = () => {
   const [user, setUser] = useState(null);
-  const [teachersData, setTeachersData] = useState(null)
+  const [teachersData, setTeachersData] = useState([])
+  const [loading, setLoading] = useState(false)
 
+  // Uncomment and use this if you need to check user info from session storage
   // useEffect(() => {
   //   const userInfoString = sessionStorage.getItem('userInfo');
   //   if (userInfoString) {
@@ -23,8 +25,15 @@ const page = () => {
 
   useEffect(()=>{
     const fetchTeachers = async () => {
-      const res = await mockTeachersData();
-      setTeachersData(res);
+      setLoading(true)
+      try {
+        const res = await mockTeachersData();
+        setTeachersData(res);
+      } catch (error) {
+        console.log('Failed to fetch teachers: ' + error)
+      } finally {
+        setLoading(false)
+      }
     }
     fetchTeachers();
   },[])
@@ -37,47 +46,51 @@ const page = () => {
       </div>
 
       <div className="relative rounded-lg">
-          <input className="rounded-md border px-4 py-2 pl-10 text-indigo-600  focus:outline-none focus:border-indigo-500 w-full" placeholder="Search for teacher by name or email" />
+          <input className="rounded-md border px-4 py-2 pl-10 text-indigo-600 focus:outline-none focus:border-indigo-500 w-full" placeholder="Search for teacher by name or email" />
           <BiSolidSearchAlt2 className="absolute left-[10px] top-[50%] -translate-y-[50%] text-indigo-600 text-[18px]" />
       </div>
 
-      <div className="overflow-x-auto">
-        {teachersData > 0 ? (
-      <>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Name</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Subject</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Class</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Email Address</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Gender</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Salary</th>
-            <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Phone Number</th>
-          </tr>
-        </thead>
+      {loading ? <div>Loading</div>
+       : 
+      (
+        <div className="overflow-x-auto">
+        {teachersData.length > 0 ? (
+          <>
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Name</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Subject</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Class</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Email Address</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Gender</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Salary</th>
+                  <th className="px-6 py-3 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider">Phone Number</th>
+                </tr>
+              </thead>
 
-        <tbody className="bg-white divide-y divide-gray-200">
-          {teachersData.map((row, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-violet-50'}>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-indigo-600">{row.name}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.subject}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.class}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.email}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.gender}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.salary}</td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.phoneNumber}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </>
-        )
-         : 
-        (<div className='text-gray-300 text-[15px]'>No teachers found!</div>)}
-    </div>
+              <tbody className="bg-white ">
+                {teachersData.map((row, index) => (
+                  <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-violet-50'}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-indigo-600">{row.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.subject}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.class}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.email}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.gender}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.salary}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-indigo-500">{row.phoneNumber}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : (
+          <div className='text-gray-300 text-[15px]'>No teachers found!</div>
+        )}
+      </div>
+      )}
     </div>
   )
 }
 
-export default page
+export default Page;
--- src/utils/mockTeachersData.jsx
+++ src/utils/mockTeachersData.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-
 const mockTeachersData = () => {
-    const data = [
-        {
-          name: 'John Doe',
-          subject: 'Mathematics',
-          class: '10A',
-          email: 'john.doe@example.com',
-          gender: 'Male',
-          salary: '$5000',
-          phoneNumber: '123-456-7890',
-        },
-        {
-          name: 'Jane Smith',
-          subject: 'English',
-          class: '10B',
-          email: 'jane.smith@example.com',
-          gender: 'Female',
-          salary: '$5500',
-          phoneNumber: '123-456-7891',
-        },
-        
-      ];
-  return new Promise((resolve)=>{
-    setTimeout(()=>{
-      resolve(data)
-    }, 1000)
-  });
-}
+  const data = [
+    {
+      name: 'John Doe',
+      subject: 'Mathematics',
+      class: '10A',
+      email: 'john.doe@example.com',
+      gender: 'Male',
+      salary: '₵ 5000',
+      phoneNumber: '123-456-7890',
+    },
+    {
+      name: 'Jane Smith',
+      subject: 'English',
+      class: '10B',
+      email: 'jane.smith@example.com',
+      gender: 'Female',
+      salary: '₵ 5500',
+      phoneNumber: '123-456-7891',
+    },
+  ];
 
-export default mockTeachersData
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 1000);
+  });
+};
+
+export default mockTeachersData;
